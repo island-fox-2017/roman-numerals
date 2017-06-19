fbe8448c3da6ba5eaf0500bc925aef7472aedf90
msrabbani <msrabbanii@gmail.com>
2017-06-19 16:45:17 +0700

    Roman Numerals sudah selesai

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,7 +1,7 @@
 function to_roman(num) {
   var result = '';
-  var decimal = [1000, 500, 400, 100, 50, 40, 10, 9, 5, 4, 1];
-  var roman = ["M", "D", "CD","C", "L","XL", "X","IX", "V","IV","I"];
+  var decimal = [1000,900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["M", "CM","D", "CD" ,"C" ,"XC" , "L","XL", "X","IX", "V","IV","I"];
 
   for (var i = 0; i < decimal.length; i++) {
     while (num%decimal[i]<num) {
@@ -20,3 +20,4 @@ console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
+console.log('99    | XCIX     | ', to_roman(99))

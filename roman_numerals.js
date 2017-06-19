b37f08d6f479cc3d62acca9442c5a6dce5db3585
biliman <fickry.bil.iman@gmail.com>
2017-06-19 11:30:06 +0700

    roman numerals done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,11 +1,24 @@
 function to_roman (num) {
   // your implementation code here
+  let latin = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  let result = '';
+  
+  
+  for (let i = 0; i < latin.length; i++) {
+    while (latin[i] <= num) {
+      result += roman[i]
+      num -= latin[i]
+    }
+  }
+  return result;
 }
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|———————')
+console.log('3     | III      | ', to_roman(3))
 console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))

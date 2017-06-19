3c55b2440a5528ea0117bfac4c6690f94ff0782b
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-06-19 14:44:42 +0700

    upload tugas roman numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,6 +1,41 @@
-function to_roman (num) {
-  // your implementation code here
+var romanMatrix = [
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [3, 'III'],
+  [1, 'I']
+];
+
+function to_roman(num){
+  var tampung = [];
+  for(var i = 0; i < romanMatrix.length; i++)
+  {
+    if(num >= romanMatrix[i][0])
+    {
+      tampung.push(romanMatrix[i][1]);
+      if(num % romanMatrix != 0)
+      {
+          num = num % romanMatrix[i][0];
+      }
+      else
+      {
+          num = num / romanMatrix[i][0];
+      }
+    }
+  }
+  return tampung.join('');
 }
+
+
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')

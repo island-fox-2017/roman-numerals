d9de5592135dad6cd7554159327753b5283ae7f2
gananggww <gananggww@gmail.com>
2017-06-19 22:24:54 +0700

    Update tugas roman number

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,8 +1,25 @@
-function to_roman (num) {
-  // your implementation code here
+
+
+function to_roman(input) {
+  var nums = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+  var romans = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+  var result = "";
+
+  for(let i=0; i< nums.length; i++) { 
+    if(input >= nums[i]) {
+      while(input >= nums[i]) {
+        result += romans[i];
+        input = input - nums[i];
+      }
+
+    }
+  }
+  return result
 }
 
 // Drive code
+// console.log(to_roman(1245));
+
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|———————')

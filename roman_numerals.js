00f15221b83b8730ff1f5d83768590f6e5c0e804
tezaharsony <teza.harsony2303942gmail.com>
2017-06-19 16:32:34 +0700

    pengumpulan tugas

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,13 @@
 function to_roman (num) {
-  // your implementation code here
+  var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+  var to_roman = "",i;
+ for ( i in lookup ) {
+   while ( num >= lookup[i] ) {
+     to_roman += i;
+     num -= lookup[i];
+   }
+ }
+ return to_roman
 }
 
 // Drive code

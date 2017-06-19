a49740c6b67c5ab86407b767ec7171c67a7c1a03
adnin31 <adnin.ris31@gmail.com>
2017-06-19 16:22:46 +0700

    git roman-numerals telah dikerjakan

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,53 @@
 function to_roman (num) {
   // your implementation code here
+  roman=[]
+  if(num <3000){
+    while(num > 0){
+      if (num>0 && num <4) {
+        roman.push('I')//ubah satuan
+        num --
+      } else if(num == 4 ){ //ubah empat
+        roman.push('IV')
+        num -=4
+      } else if (num >=5 && num <9) {// ubah lima
+        roman.push('V')
+        num -=5
+      } else if (num >=9 && num <10) { //ubah sembilan
+        roman.push('IX')
+        num -=9
+      }  else if (num> 9 && num <40 ) {//ubah puluhan
+        roman.push('X')
+        num -=10
+      } else if (num >=40 && num <50) {//ubah empatpuluhan
+        roman.push('XL')
+        num -=40
+      } else if (num > 49 && num <90) {//ubah limapuluhan
+        roman.push('L')
+         num -= 50;
+      } else if (num >=90 && num<100) { //ubah sembilanPuluhan
+          roman.push('xc')
+         num -= 90;
+      } else if (num >=100 && num <400) {//ubah ratusan
+        roman.push('C')
+        num -=100
+      } else if ( num >= 400 && num <500) {//ubah empatRatusan
+        roman.push('CD')
+        num-=400
+      } else if (num >= 500 && num < 900) {//ubah limaRatusan
+        roman.push('D')
+        num -= 500
+      }else if (num >=900 && num <1000) {//ubah sembilanRatussan
+        roman.push('DM')
+        num-=900;
+      }
+      else if (num >=1000) {//ubah ribuan
+        roman.push('M')
+        num -= 1000
+      }
+    }
+  }
+  return roman.join('');
+
 }
 
 // Drive code

f66ac98bb99cd15b2122e5cb2fa58e2cc7586cfa
adnin31 <adnin.ris31@gmail.com>
2017-06-20 11:31:39 +0700

    update roman num

--- roman_numerals .js
+++ roman_numerals .js
@@ -3,14 +3,17 @@ function to_roman (num) {
   let romanize =''
   let listRoman = ['M','DM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
   let number = [1000 ,900,500,400,100,90,50,40,10,9,5,4,1];
+if(num <=3000){
   for (var i = 0; i < listRoman.length; i++) {
-    if (num >= number[i]) {
       while (num >= number[i]) {
         romanize += listRoman[i]
         num -= number[i]
-    }
-  }
+      }
  }
+} else {
+  romanize = 'Input harus kurang dari 3000'
+}
+
   return romanize;
 }
 
@@ -22,4 +25,4 @@ console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+console.log('1646  | MDCXLVI  | ', to_roman(3001))
